49aea348fb93a629fc2d6277aa88ba2ae057b35a
ItsTrudeauver <smz.osaragi@gmail.com>
2025-01-27 21:42:47 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="refresh" content="0; URL='C0R3/gateway.html'" />
+    <title>Redirecting...</title>
+</head>
+<body>
+    If you are not redirected, <a href="C0R3/tabs/gateway.html">click here</a>.
+</body>
+</html>
